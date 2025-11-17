cd464c24e6710b1d3f678300eac488e3e564ba87
JesusFTC <jajaxd5081@gmail.com>
2025-11-16 22:29:13 -0600

    "Initial commit"

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ManualRobot2P.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ManualRobot2P.java
@@ -10,9 +10,8 @@ import com.qualcomm.robotcore.hardware.Servo;
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 
 @TeleOp(name = "RobotManual")
-public class ManualRobot extends OpMode {
-    //Recordatorio: Evitar o eliminar lineas de codigo u objetos que no se usan
-    //En el codigo completo, esto puede generar delay a la hora de ejecutar el codigo.
+public class ManualRobot2P extends OpMode {
+
     //---------------------------C-h-a-s-i-s--------------------------
     DcMotorEx m_fl;
     DcMotorEx m_fr;
@@ -21,10 +20,10 @@ public class ManualRobot extends OpMode {
     DcMotorEx m_intake;
     DcMotorEx m_shooter1;
     DcMotorEx m_shooter2;
-    public Servo Servo90;
+    //public Servo Servo90;
     IMUBench bench = new IMUBench();
 
-    double power = 0.6;
+    double power = 0;
 
 
     @Override
@@ -39,7 +38,6 @@ public class ManualRobot extends OpMode {
         m_intake = hardwareMap.get(DcMotorEx.class, "IntakeMotor");
         m_shooter1 = hardwareMap.get(DcMotorEx.class, "ShooterMotorA");
         m_shooter2 = hardwareMap.get(DcMotorEx.class, "ShooterMotorB");
-        Servo90 = hardwareMap.get(Servo.class, "Servo");
 
         bench.init(hardwareMap);
 
@@ -51,7 +49,6 @@ public class ManualRobot extends OpMode {
         m_intake.setMode(DcMotorEx.RunMode.RUN_USING_ENCODER);
 
         //--------------C-o-n-f-i-g-u-r-a-c-i-ó-n d-e P-I-D------------------
-        //m_shooter.setVelocityPIDFCoefficients(10,0,0,12);
 
         m_fr.setDirection(DcMotorSimple.Direction.REVERSE);
         m_bl.setDirection(DcMotorSimple.Direction.FORWARD);
@@ -66,35 +63,26 @@ public class ManualRobot extends OpMode {
         telemetry.addData("Current Orientation:", bench.getHeading(AngleUnit.DEGREES));
         telemetry.addData("Shooter Velocity:", m_shooter2.getVelocity());
         telemetry.addData("Intake Velocity:", m_intake.getVelocity());
-        telemetry.addData("Posicion:", Servo90.getPosition());
+       //telemetry.addData("Posicion:", Servo90.getPosition());
         telemetry.update();
 
         double y = gamepad1.left_stick_y;
         double x = -gamepad1.left_stick_x;
         double rx = gamepad1.right_stick_x;
-        //Ejemplo : 200 = 22RPM
         double DesearedVelocity = 1900; //1850
 
 
         //----------------------C-h-a-s-i-s-----------------------
 
-        // Se convierte de grados a radianes por qué al usar Math.sin o Math.cos, ambos de estos
-        //Utilizan radianes. . .
         double botHeading = Math.toRadians(bench.getHeading(AngleUnit.DEGREES));
 
-        // Se rota el vector (Direccion y magnitud) del joystick según la orientyación del robot
         double rotX = x * Math.cos(-botHeading) - y * Math.sin(-botHeading);
         double rotY = x * Math.sin(-botHeading) + y * Math.cos(-botHeading);
 
-        // Se calcula la potencia de cada motor, usando las variables rotX y rotY
         double FLpower = rotY + rotX - rx;
         double BLpower = rotY - rotX - rx;
         double FRpower = rotY - rotX + rx;
         double BRpower = rotY + rotX + rx;
-
-        /*Se utiliza un max y un abs, el max para hacer que los datos no sobresalgan de 1
-            Y el abs, para darlo como un numero positivo siempre.
-         */
 
         double MaxPower = Math.max(1.0, Math.max(Math.abs(FLpower),
                 Math.max(Math.abs(FRpower), Math.max(Math.abs(BLpower), Math.abs(BRpower)))));
@@ -107,7 +95,7 @@ public class ManualRobot extends OpMode {
 
         //----------------------M-e-c-h-a-n-i-s-m-s-----------------------
 
-        if (gamepad1.left_trigger >= 0.1) {
+        if (gamepad2.left_trigger >= 0.1) {
             m_intake.setPower(-0.9);
         }
         else {
@@ -115,7 +103,7 @@ public class ManualRobot extends OpMode {
         }
 
 
-        if (gamepad1.right_trigger >= 0.1) {
+        if (gamepad2.right_trigger >= 0.1) {
             m_shooter1.setPower(-gamepad1.right_trigger);
             m_shooter2.setPower(gamepad1.right_trigger);
         } else {
@@ -125,16 +113,16 @@ public class ManualRobot extends OpMode {
 
 
         //Reseteo de frente
-        if (gamepad1.x) {
+        if (gamepad2.x) {
             bench.resetImu();
         }
 
         //Poder del motor
-        if (gamepad1.b) {
+        if (gamepad2.b) {
             power = 0.40;
         }
-        if (gamepad1.a) {
-            power = 0.6;
+        if (gamepad2.a) {
+            power = 0.5;
         }
     }
 }
